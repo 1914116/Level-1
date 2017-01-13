49d96eff82bbe57b86504329da80d9024b64ebef
1914116 <league@WTS-iMac-11.attlocal.net>
2017-01-12 19:53:38 -0800

    STUF

---
+++ src/Graphics/TypingTutor.java
@@ -0,0 +1,79 @@
+package Graphics;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Date;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class TypingTutor implements KeyListener {
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+	char currentLetter = generateRandomLetter();
+	int Count = 0;
+	public static void main(String[] args) {
+		TypingTutor t = new TypingTutor();
+		t.setup();
+	}
+	public void setup(){
+		frame = new JFrame();
+		panel = new JPanel();
+		label = new JLabel();
+		frame.add(panel);
+		panel.add(label);
+		label.setText(""+ currentLetter);
+		frame.setVisible(true);
+		label.setFont(label.getFont().deriveFont(28.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		frame.addKeyListener(this);
+		frame.pack();
+	}
+	char generateRandomLetter() {
+		Random r = new Random();
+		return (char) (r.nextInt(26) + 'a');
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+	System.out.println(e.getKeyChar());
+		if(e.getKeyChar()== currentLetter){
+			System.out.println("correct");
+	panel.setBackground(Color.GREEN);
+		Count++;	
+		}
+		else{
+			panel.setBackground(Color.RED);
+			
+		}
+		if(Count>=10){
+			System.out.println(Count);
+		}
+	}
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		currentLetter = generateRandomLetter();
+		label.setText(""+ currentLetter);
+	}
+	private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
+		Date timeAtEnd = new Date();
+		long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+		long gameInSeconds = (gameDuration / 1000) % 60;
+		double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
+		int charactersPerMinute = (int) (charactersPerSecond * 60);
+		JOptionPane.showMessageDialog(null, "Your typing speed is " + charactersPerMinute + " characters per minute.");
+	}
+	Date timeAtStart = new Date();
+
+
+}
